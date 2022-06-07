382e20ec8c941fbaa69c01d123f3eb38bac832ce
blatnak <katerina.blatna@uzis.cz>
2022-06-07 15:14:51 +0200

    count function added

--- main.js
+++ main.js
@@ -6,3 +6,11 @@ nadpis.classList.add("zluty")
 let ctverecek = document.querySelector('div.zluty')
 ctverecek.classList.add("ctverecek")
 
+let count = 0;
+
+function priNajetiNaCtverec() {
+    console.log(`Počet najetí na čtverec je ${count}`)
+    //count += 1 nebo
+    //count = count + 1
+    count++
+}
