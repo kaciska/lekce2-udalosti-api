b51aba5d9acf3e46d8816b25b64216654bc3e1b0
blatnak <katerina.blatna@uzis.cz>
2022-06-07 15:04:13 +0200

    script tag into html & basic functions added

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 // tady je místo pro náš program
+
+let nadpis = document.querySelector('h1')
+nadpis.classList.add("zluty")
+
+let ctverecek = document.querySelector('div.zluty')
+ctverecek.classList.add("ctverecek")
+
