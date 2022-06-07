b3f1ca980467412a88aa6d7b6133a3bc5ab2c89c
blatnak <katerina.blatna@uzis.cz>
2022-06-07 15:21:07 +0200

    adding heading toggle onclick

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 // tady je místo pro náš program
 
 let nadpis = document.querySelector('h1')
-nadpis.classList.add("zluty")
+nadpis.classList.add('zluty')
 
 let ctverecek = document.querySelector('div.zluty')
-ctverecek.classList.add("ctverecek")
+ctverecek.classList.add('ctverecek')
 
-let count = 0;
+let count = 0
 
 function priNajetiNaCtverec() {
-    console.log(`Počet najetí na čtverec je ${count}`)
-    //count += 1 nebo
-    //count = count + 1
-    count++
+  console.log(`Počet najetí na čtverec je ${count}`)
+  //count += 1 nebo
+  //count = count + 1
+  count++
+}
+
+function zmenStyl() {
+  nadpis.classList.toggle('zeleny')
 }
